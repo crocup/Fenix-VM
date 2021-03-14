7c84b31642875c8b2c209b23e21664589148b9f0
dimon <patunutap@gmail.com>
2021-03-14 21:23:39 +0300

    feat: Добавлен класс vulnerability fix: Добавлена возможность обнаружения уязвимостей в базах vulnd и cve mitre

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
 HOST_DISCOVERY = 'http://127.0.0.1:9001/api/v1/discovery'
 API_DATABASE = 'http://127.0.0.1:9002/api/v1/database/mongo'
 GET_ALL_DATABASE = 'http://127.0.0.1:9002/api/v1/database/mongo/get_all'
+API_VULNDB = 'api'
