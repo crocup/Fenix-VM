af068e197cf4d37bb0ced7c8519529bb8903f83b
darkwing <patunutap@gmail.com>
2021-08-07 17:18:37 +0300

    feat: create monolit app fix: add docs

--- fsec/__init__.py
+++ fsec/__init__.py
@@ -1,19 +1,11 @@
+import os
 from flask import Flask
-from flask_migrate import Migrate, MigrateCommand
-from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager, Command, Shell
-from flask_login import LoginManager
-import os, config
 # https://pythonru.com/uroki/19-struktura-i-jeskiz-prilozhenija-flask
 # создание экземпляра приложения
 fsec = Flask(__name__)
-fsec.config.from_object(os.environ.get('FLASK_ENV') or 'config.DevelopementConfig')
+# fsec.config.from_object(os.environ.get('FLASK_ENV') or 'config.DevelopementConfig')
 
-# инициализирует расширения
-manager = Manager(fsec)
-db = SQLAlchemy(fsec)
-migrate = Migrate(fsec,  db)
-manager.add_command('db', MigrateCommand)
+
 
 # import views
 from . import views
