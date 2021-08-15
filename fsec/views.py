5aa2922758d6b5f2d6619690d22e368b30d3c2c9
darkwing <patunutap@gmail.com>
2021-08-15 22:17:54 +0300

    fix: create base page

--- fsec/views.py
+++ fsec/views.py
@@ -7,10 +7,16 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @login_required
 def index():
-    return render_template('index.html', name=current_user.name)
+    return render_template('index.html')
 
 
 @main.route('/about')
 @login_required
 def about():
-    return render_template('about.html', name=current_user.name)
+    return render_template('about.html')
+
+
+@main.route('/inventory')
+@login_required
+def inventory():
+    return render_template('inventory.html')
