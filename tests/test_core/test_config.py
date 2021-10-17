afd25d92f836ce5e5aa16584e1140c095f25f8f2
darkwing <patunutap@gmail.com>
2021-10-17 16:33:07 +0300

    Refactoring FSEC * create new test * update README.md * added dependencies: coverage 6.0.2, pytest-cov 3.0, tomli 1.2.1

---
+++ tests/test_core/test_config.py
@@ -0,0 +1,16 @@
+from app.core.config import API_PREFIX, PROJECT_NAME, DATABASE_IP, DATABASE_PORT, VERSION
+from app.core.setting import Settings
+
+
+def test_data_config_default():
+    assert API_PREFIX == "/api/v1"
+    assert PROJECT_NAME == "FSEC VM"
+    assert VERSION == "0.0.19"
+    assert DATABASE_IP == "127.0.0.1"
+    assert DATABASE_PORT == 27017
+
+
+def test_setting():
+    assert Settings().app_name == PROJECT_NAME
+    assert Settings().app_version == VERSION
+    assert Settings().admin_email == "patunutap@gmail.com"
